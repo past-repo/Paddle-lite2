f3b177d93aa1f6e2c8a84d06025c852c0b87e131
tensor-tang <tangjian03@baidu.com>
2018-09-04 17:49:25 +0800

    comment dot graph

--- paddle/fluid/inference/analysis/analyzer.cc
+++ paddle/fluid/inference/analysis/analyzer.cc
@@ -106,13 +106,13 @@ void Analyzer::Run(Argument* argument) {
   argument->Set(kFluidToIrPassesAttr,
                 new std::vector<std::string>({
                     // Manual update the passes here.
-                    "graph_viz_pass",                            //
-                    "infer_clean_graph_pass", "graph_viz_pass",  //
-                    // "attention_lstm_fuse_pass", "graph_viz_pass",  //
-                    "fc_lstm_fuse_pass", "graph_viz_pass",        //
-                    "mul_lstm_fuse_pass", "graph_viz_pass",       //
-                    "seq_concat_fc_fuse_pass", "graph_viz_pass",  //
-                    "fc_fuse_pass", "graph_viz_pass"              //
+                    //                    "graph_viz_pass",
+                    "infer_clean_graph_pass",  // "graph_viz_pass",
+                    // "attention_lstm_fuse_pass", "graph_viz_pass",
+                    "fc_lstm_fuse_pass",        // "graph_viz_pass",
+                    "mul_lstm_fuse_pass",       // "graph_viz_pass",
+                    "seq_concat_fc_fuse_pass",  // "graph_viz_pass",
+                    "fc_fuse_pass"              // , "graph_viz_pass"
                 }));
 
   for (auto& x : data_) {
