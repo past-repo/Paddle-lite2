4a844f3eb3928181a6af6f9a8d97224cb811f0b9
superjomn <yanchunwei@outlook.com>
2019-01-28 10:36:15 +0800

    up

--- paddle/fluid/inference/tests/api/vis_2.cc
+++ paddle/fluid/inference/tests/api/vis_2.cc
@@ -22,33 +22,39 @@ TEST(vis, multi_thread) {
   std::vector<size_t> latencies;
   inference::Timer timer;
 
+  std::vector<std::vector<PaddleTensor>> input_datas;
+
+  for (int i = 0; i < 1000; i++) {
+    input_datas.emplace_back();
+    auto& inputs = input_datas.back();
+    inputs.resize(2);
+
+    auto& tensor0 = inputs[0];
+    auto& tensor1 = inputs[1];
+
+    tensor0.shape.assign({1, 3, 225, 300});
+    tensor0.data.Resize(1 * 3 * 225 * 300 * sizeof(float));
+
+    for (int i = 0; i < tensor0.data.length() / sizeof(float); i++) {
+      static_cast<float*>(tensor0.data.data())[i] =
+          rand() / RAND_MAX * 256 - 127;
+    }
+
+    tensor1.shape.assign({3, 1});
+    tensor1.data.Resize(3 * 1 * sizeof(float));
+    auto* tensor1_data = static_cast<float*>(tensor1.data.data());
+    tensor1_data[0] = 2.25e2;
+    tensor1_data[1] = 3e2;
+    tensor1_data[2] = 2.9e-1;
+  }
+
   for (int i = 0; i < 3; i++) {
     threads.emplace_back([&] {
       auto child = predictor->Clone();
       // auto child = CreatePaddlePredictor(config);
       // auto child = CreatePaddlePredictor(config);
-      for (int j = 0; j < 10000; j++) {
-        std::vector<PaddleTensor> inputs, outputs;
-        inputs.resize(2);
-
-        auto& tensor0 = inputs[0];
-        auto& tensor1 = inputs[1];
-
-        tensor0.shape.assign({1, 3, 225, 300});
-        tensor0.data.Resize(1 * 3 * 225 * 300 * sizeof(float));
-
-        for (int i = 0; i < tensor0.data.length() / sizeof(float); i++) {
-          static_cast<float*>(tensor0.data.data())[i] =
-              rand() / RAND_MAX * 256 - 127;
-        }
-
-        tensor1.shape.assign({3, 1});
-        tensor1.data.Resize(3 * 1 * sizeof(float));
-        auto* tensor1_data = static_cast<float*>(tensor1.data.data());
-        tensor1_data[0] = 2.25e2;
-        tensor1_data[1] = 3e2;
-        tensor1_data[2] = 2.9e-1;
-
+      for (auto& inputs : input_datas) {
+        std::vector<PaddleTensor> outputs;
         timer.tic();
         ASSERT_TRUE(child->Run(inputs, &outputs));
         latencies.push_back(timer.toc());
